3959f886a473a5a718672ce8b29667c71d8ced81
Ion <teloslinux@protonmail.com>
2023-10-02 17:48:06 +0300

    Add files via upload

---
+++ get-favicon.py
@@ -0,0 +1,20 @@
+import requests
+import favicon
+import subprocess
+import sys
+
+print("example how to call this program: get-favicon https://debian.org debian-favicon ~/Downloads \nMake sure you install icoutils to convert .ico output files to .png")
+
+icons = favicon.get(sys.argv[1])
+icon = icons[0]
+
+response = requests.get(icon.url, stream=True)
+with open(sys.argv[3] + '/' + sys.argv[2] + '.' + format(icon.format), 'wb') as image:
+    for chunk in response.iter_content(1024):
+        image.write(chunk)
+        
+if format(icon.format) == "ico":
+    subprocess.run(["sudo", "apt-get", "install", "icoutils", "-y"])
+    subprocess.run(["icotool", "-x", "-o", sys.argv[3], sys.argv[3]+"/"+sys.argv[2]+".ico"])
+    print("converted .ico file to .png for more convenient use in Linux .desktop")
+
